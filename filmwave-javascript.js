e4bc2d78219ad1bc95edb61abe5a70b5c44008d3
filmwave-webmaster <filmwave.webmaster@gmail.com>
2025-12-30 07:08:30 -0800

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -1055,22 +1055,12 @@ function initializeWaveforms() {
   });
   
   songCards.forEach((cardElement) => {
-  const isInTemplate = cardElement.closest('.template-wrapper');
-  const hasNoData = !cardElement.dataset.audioUrl || !cardElement.dataset.songId;
-  
-  if (isInTemplate || hasNoData) {
-    return;
-  }
-
-  // Check if this is the currently playing song - prioritize it!
-  const songId = cardElement.dataset.songId;
-  const isCurrentSong = g.currentSongData && g.currentSongData.id === songId;
-  
-  if (isCurrentSong) {
-    console.log('✅ FOUND CURRENT SONG - PRIORITIZING:', songId);
-    visibleCards.push(cardElement);
-    return;
-  }
+    const isInTemplate = cardElement.closest('.template-wrapper');
+    const hasNoData = !cardElement.dataset.audioUrl || !cardElement.dataset.songId;
+    
+    if (isInTemplate || hasNoData) {
+      return;
+    }
     
     const waveformContainer = cardElement.querySelector('.waveform');
     if (waveformContainer) {
