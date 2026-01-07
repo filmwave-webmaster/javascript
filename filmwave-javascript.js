7729e569ec24066a110d47d56f5e0ea735955682
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-01-07 15:45:04 -0800

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -1074,11 +1074,6 @@ function playStandaloneSong(audioUrl, songData, wavesurfer, cardElement, seekToT
   });
   
   createStandaloneAudio(audioUrl, songData, wavesurfer, cardElement, seekToTime, shouldAutoPlay);
-  
-  // Update player visibility and footer padding
-  setTimeout(() => {
-    updateMasterPlayerVisibility();
-  }, 100);
 }
 
 /**
