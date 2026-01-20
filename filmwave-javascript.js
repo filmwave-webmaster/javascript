232556a61d778921da5025788b9f7850fe3f6830
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-01-20 01:40:37 -0800

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -4661,6 +4661,18 @@ if (typeof barba !== 'undefined') {
 },
 
      beforeEnter(data) {
+
+       const g = window.musicPlayerPersistent;
+  
+  // Capture sidebar from incoming page BEFORE Barba processes it
+  const incomingSidebar = data.next.container.querySelector('.sidebar-nav');
+  
+  if (incomingSidebar && !g.sidebarClone) {
+    g.sidebarClone = incomingSidebar.cloneNode(true);
+    console.log('💾 [BEFORE ENTER] Stored sidebar clone from incoming page');
+  }
+},
+      
   const nextContainer = data.next.container;
   const isMusicPage = !!nextContainer.querySelector('.music-list-wrapper');
        
