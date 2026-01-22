0088560904cfedd339d6cd98e2eba0433fb4b5d8
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-01-22 04:01:19 -0800

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -8469,7 +8469,7 @@ async function initDashboardPlaylists() {
         transition: margin-top 200ms ease !important;
       }
       .${COMPACT_CLASS} .nav-logo {
-        width: 165px !important;
+        width: 150px !important;
         transition: width 200ms ease !important;
       }
 
