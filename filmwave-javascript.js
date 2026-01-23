2a556e9aaea4cd99a821e0b7142741199b083cf1
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-01-23 03:48:32 -0800

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -8519,7 +8519,7 @@ container.querySelectorAll('.playlist-card-template:not(.is-template)').forEach(
         transition: margin-top 200ms ease !important;
       }
       .${COMPACT_CLASS} .nav-logo {
-        width: 150px !important;
+        width: 175px !important;
         transition: width 200ms ease !important;
       }
 
