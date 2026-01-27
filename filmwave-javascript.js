4b6bf4269d44f39713c49bb292f7ea7cd9290741
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-01-27 03:07:42 -0800

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -2169,7 +2169,7 @@ function initCheckboxTextColor() {
     checkbox.addEventListener('change', function() {
       const label = this.parentElement.querySelector('.w-form-label');
       if (label) {
-        label.style.color = this.checked ? 'var(--color-6)' : '';
+        label.style.color = 'var(--color-6)';
       }
     });
   });
