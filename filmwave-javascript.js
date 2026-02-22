04872ebd5a641bc14f9e4030ba08a963cea9e924
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-02-22 06:16:48 -0800

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -1378,31 +1378,24 @@ if (playerCoverArt) {
   const isMusicPage = !!document.querySelector('.music-list-wrapper');
   
   if (isMusicPage && g.allWavesurfers.length > 0 && g.currentWavesurfer) {
-    // Get visible cards in DOM order (respects shuffle)
-    const container = document.querySelector('.music-list-wrapper');
-    const visibleCards = Array.from(container?.querySelectorAll('.song-wrapper:not(.template-wrapper .song-wrapper)') || [])
-      .filter(card => card.style.display !== 'none');
-    
-    // Find current song's card and index in DOM order
-    const currentSongId = g.currentSongData?.id;
-    const currentCardIndex = visibleCards.findIndex(card => card.dataset.songId === currentSongId);
-    
+    const currentIndex = g.allWavesurfers.indexOf(g.currentWavesurfer);
     let targetWS = null;
-    let targetCardIndex = -1;
     
     if (direction === 'next') {
-      targetCardIndex = currentCardIndex + 1;
-      if (targetCardIndex >= visibleCards.length) targetCardIndex = -1;
+      for (let i = currentIndex + 1; i < g.allWavesurfers.length; i++) {
+        const data = g.waveformData.find(d => d.wavesurfer === g.allWavesurfers[i]);
+        if (data && data.cardElement.offsetParent !== null) {
+          targetWS = g.allWavesurfers[i];
+          break;
+        }
+      }
     } else {
-      targetCardIndex = currentCardIndex - 1;
-    }
-    
-    if (targetCardIndex >= 0 && targetCardIndex < visibleCards.length) {
-      const targetCard = visibleCards[targetCardIndex];
-      const targetSongId = targetCard.dataset.songId;
-      const targetData = g.waveformData.find(d => d.songData?.id === targetSongId);
-      if (targetData) {
-        targetWS = targetData.wavesurfer;
+      for (let i = currentIndex - 1; i >= 0; i--) {
+        const data = g.waveformData.find(d => d.wavesurfer === g.allWavesurfers[i]);
+        if (data && data.cardElement.offsetParent !== null) {
+          targetWS = g.allWavesurfers[i];
+          break;
+        }
       }
     }
     
