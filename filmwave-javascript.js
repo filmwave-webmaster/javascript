dd2b2244f6b878358b32c15d5b1f3661646ce529
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-02-22 06:19:24 -0800

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -1378,28 +1378,32 @@ if (playerCoverArt) {
   const isMusicPage = !!document.querySelector('.music-list-wrapper');
   
   if (isMusicPage && g.allWavesurfers.length > 0 && g.currentWavesurfer) {
-    const currentIndex = g.allWavesurfers.indexOf(g.currentWavesurfer);
-    let targetWS = null;
+    // Get visible cards in DOM order (respects shuffle)
+    const container = document.querySelector('.music-list-wrapper');
+    const visibleCards = Array.from(container?.querySelectorAll('.song-wrapper:not(.template-wrapper .song-wrapper)') || [])
+      .filter(card => card.style.display !== 'none');
+    
+    // Find current song's card and index in DOM order
+    const currentSongId = g.currentSongData?.id;
+    const currentCardIndex = visibleCards.findIndex(card => card.dataset.songId === currentSongId);
+    
+    let nextData = null;
+    let targetCardIndex = -1;
     
     if (direction === 'next') {
-      for (let i = currentIndex + 1; i < g.allWavesurfers.length; i++) {
-        const data = g.waveformData.find(d => d.wavesurfer === g.allWavesurfers[i]);
-        if (data && data.cardElement.offsetParent !== null) {
-          targetWS = g.allWavesurfers[i];
-          break;
-        }
-      }
+      targetCardIndex = currentCardIndex + 1;
+      if (targetCardIndex >= visibleCards.length) targetCardIndex = -1;
     } else {
-      for (let i = currentIndex - 1; i >= 0; i--) {
-        const data = g.waveformData.find(d => d.wavesurfer === g.allWavesurfers[i]);
-        if (data && data.cardElement.offsetParent !== null) {
-          targetWS = g.allWavesurfers[i];
-          break;
-        }
-      }
+      targetCardIndex = currentCardIndex - 1;
     }
     
-    if (targetWS) {
+    if (targetCardIndex >= 0 && targetCardIndex < visibleCards.length) {
+      const targetCard = visibleCards[targetCardIndex];
+      const targetSongId = targetCard.dataset.songId;
+      nextData = g.waveformData.find(d => d.songData?.id === targetSongId);
+    }
+    
+    if (nextData && nextData.wavesurfer) {
       const wasPlaying = g.isPlaying;
       const prevData = g.waveformData.find(data => data.wavesurfer === g.currentWavesurfer);
       if (prevData?.cardElement.querySelector('.play-button')) {
@@ -1412,7 +1416,6 @@ if (playerCoverArt) {
       }
       
       g.currentWavesurfer.seekTo(0);
-      const nextData = g.waveformData.find(data => data.wavesurfer === targetWS);
       
       if (nextData?.cardElement.querySelector('.play-button')) {
         nextData.cardElement.querySelector('.play-button').style.opacity = '1';
@@ -1430,7 +1433,7 @@ if (playerCoverArt) {
         songWrapper.style.setProperty('border', '1px solid var(--color-8)', 'important');
       }
       
-      playStandaloneSong(nextData.audioUrl, nextData.songData, targetWS, nextData.cardElement, null, wasPlaying);
+      playStandaloneSong(nextData.audioUrl, nextData.songData, nextData.wavesurfer, nextData.cardElement, null, wasPlaying);
     }
   } else {
     // Not on music page or no waveforms - use standalone navigation
