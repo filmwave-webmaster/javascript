d1e2524eed2fa2bce4b3c7d3390467ed4edec5a2
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-04-17 17:04:10 -0700

    Update filmwave-javascript.js

--- filmwave-javascript.js
+++ filmwave-javascript.js
@@ -7371,11 +7371,13 @@ function initUniversalSearch() {
   const wrapperSelectors = [
     ".create-playlist-module-wrapper",
     ".add-to-playlist-module-wrapper"
+    ".playlist-edit-module-wrapper"
   ];
 
   const moduleSelectors = [
     ".create-playlist-module",
     ".add-to-playlist-module"
+    ".playlist-edit-module"
   ];
 
   function isActive(el) {
